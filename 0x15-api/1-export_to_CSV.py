e9a9e3de25e7817f6b7330498aa8a5bd3736be9e
oangel26 <oa.angel26@gmail.com>
2022-01-31 21:12:03 -0500

    First commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script that using REST API, for a given given employee ID, returns
+information about his/her TODO list progress in a CSV file.
+"""
+
+
+if __name__ == "__main__":
+    import csv
+    import json
+    import requests
+    import sys
+
+    if len(sys.argv) < 2:
+        print("USAGE: python3 [MODULE NAME] [EMPLOYEE ID]")
+    else:
+        response = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                                sys.argv[1])
+        employee_json = response.content
+
+        response = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                                sys.argv[1] + '/todos')
+        employee_todos_json = response.content
+
+        employee_todos_list = json.loads(employee_todos_json)
+        employee_dict = json.loads(employee_json)
+
+        with open(sys.argv[1] + '.csv', 'w', encoding='UTF8') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for i_dict in employee_todos_list:
+                csv_list = []
+                csv_list.append(sys.argv[1])
+                csv_list.append(employee_dict["username"])
+                csv_list.append(i_dict["completed"])
+                csv_list.append(i_dict["title"])
+                writer.writerow(csv_list)
