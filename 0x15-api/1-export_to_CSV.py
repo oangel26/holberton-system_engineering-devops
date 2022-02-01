fa18a164dea83cb0f131988cbc2c5073381e3bcf
oangel26 <oa.angel26@gmail.com>
2022-01-31 21:32:23 -0500

    [FIX] requests.json() instead of the json module

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,7 +7,6 @@ information about his/her TODO list progress in a CSV file.
 
 if __name__ == "__main__":
     import csv
-    import json
     import requests
     import sys
 
@@ -16,14 +15,11 @@ if __name__ == "__main__":
     else:
         response = requests.get('https://jsonplaceholder.typicode.com/users/' +
                                 sys.argv[1])
-        employee_json = response.content
+        employee_dict = response.json()
 
         response = requests.get('https://jsonplaceholder.typicode.com/users/' +
                                 sys.argv[1] + '/todos')
-        employee_todos_json = response.content
-
-        employee_todos_list = json.loads(employee_todos_json)
-        employee_dict = json.loads(employee_json)
+        employee_todos_list = response.json()
 
         with open(sys.argv[1] + '.csv', 'w', encoding='UTF8') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
