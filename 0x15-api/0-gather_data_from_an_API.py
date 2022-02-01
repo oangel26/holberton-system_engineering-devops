48b1fe433569ddad815dcb171dc78e3060a661e3
oangel26 <oa.angel26@gmail.com>
2022-01-31 19:09:11 -0500

    First commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script that using REST API, for a given given employee ID, returns \
+information about his/her TODO list progress.
+"""
+
+
+from urllib import request
+from sys import argv
+import json
+
+
+with request.urlopen('https://jsonplaceholder.typicode.com/users/' + \
+                     argv[1]) as response:
+    employee_json = response.read()
+
+
+with request.urlopen('https://jsonplaceholder.typicode.com/users/' + \
+                     argv[1] + '/todos') as response:
+    employee_todos_json = response.read()
+
+employee_todos_list = json.loads(employee_todos_json)
+employee_dict = json.loads(employee_json)
+
+
+count = 0
+for employee_todos_dict in employee_todos_list:
+    if employee_todos_dict["completed"] == True:
+        count += 1
+
+
+print("Employee {} is done with tasks({}/{}):".\
+      format(employee_dict["name"], count, len(employee_todos_list)))
+for employee_todos_dict in employee_todos_list:
+    if employee_todos_dict["completed"] == True:
+        print('\t' + employee_todos_dict["title"])
