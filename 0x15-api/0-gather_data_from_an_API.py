418e11b67b3470f8ef34443d708eb2a8b7b219e3
oangel26 <oa.angel26@gmail.com>
2022-01-31 19:29:16 -0500

    [UPD] condition to not be executed when imported added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,32 +4,34 @@ Python script that using REST API, for a given given employee ID, returns
 information about his/her TODO list progress.
 """
 
-from sys import argv
-from urllib import request
-import json
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request
+    import json
 
 
-with request.urlopen('https://jsonplaceholder.typicode.com/users/' +
-                     argv[1]) as response:
-    employee_json = response.read()
+    with request.urlopen('https://jsonplaceholder.typicode.com/users/' +
+                         argv[1]) as response:
+        employee_json = response.read()
 
 
-with request.urlopen('https://jsonplaceholder.typicode.com/users/' +
-                     argv[1] + '/todos') as response:
-    employee_todos_json = response.read()
+    with request.urlopen('https://jsonplaceholder.typicode.com/users/' +
+                         argv[1] + '/todos') as response:
+        employee_todos_json = response.read()
 
-employee_todos_list = json.loads(employee_todos_json)
-employee_dict = json.loads(employee_json)
-
-
-count = 0
-for employee_todos_dict in employee_todos_list:
-    if employee_todos_dict["completed"] is True:
-        count += 1
+    employee_todos_list = json.loads(employee_todos_json)
+    employee_dict = json.loads(employee_json)
 
 
-print("Employee {} is done with tasks({}/{}):".
-      format(employee_dict["name"], count, len(employee_todos_list)))
-for employee_todos_dict in employee_todos_list:
-    if employee_todos_dict["completed"] is True:
-        print('\t ' + employee_todos_dict["title"])
+    count = 0
+    for employee_todos_dict in employee_todos_list:
+        if employee_todos_dict["completed"] is True:
+            count += 1
+
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_dict["name"], count, len(employee_todos_list)))
+    for employee_todos_dict in employee_todos_list:
+        if employee_todos_dict["completed"] is True:
+            print('\t ' + employee_todos_dict["title"])
