5e021bd742089088649a3730b8250e03de7eec1c
oangel26 <oa.angel26@gmail.com>
2022-02-16 18:57:08 -0500

    puppet lint identation errors corrected

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # puppet scripts taht executes command to correct error
 exec { 'error_correction':
-  command     => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
-  path	      => '/usr/local/bin/:/bin/',
+  command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
