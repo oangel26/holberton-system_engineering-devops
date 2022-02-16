c6d0a70275da930ba377cc7f72f921d09f5d081c
oangel26 <oa.angel26@gmail.com>
2022-02-16 18:44:52 -0500

    comments added

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# puppet scripts taht executes command to correct error
 exec { 'error_correction':
   command     => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
   path	      => '/usr/local/bin/:/bin/',
